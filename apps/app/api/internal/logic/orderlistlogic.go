2fa641ce0e982c800ad5e6e6c44e6cece42a5fb7
qshine <qjianxun0@gmail.com>
2023-06-25 22:52:31 +0800

    order logic test

--- apps/app/api/internal/logic/orderlistlogic.go
+++ apps/app/api/internal/logic/orderlistlogic.go
@@ -2,6 +2,10 @@ package logic
 
 import (
 	"context"
+	"go-zero-mall/apps/order/rpc/order"
+	"go-zero-mall/apps/product/rpc/product"
+	"strconv"
+	"strings"
 
 	"go-zero-mall/apps/app/api/internal/svc"
 	"go-zero-mall/apps/app/api/internal/types"
@@ -24,7 +28,26 @@ func NewOrderListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *OrderLi
 }
 
 func (l *OrderListLogic) OrderList(req *types.OrderListRequest) (resp *types.OrderListResponse, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+	orderRet, err := l.svcCtx.OrderRPC.Orders(l.ctx, &order.OrdersRequest{UserId: req.UID})
+	if err != nil {
+		return nil, err
+	}
+	var pids []string
+	for _, o := range orderRet.Orders {
+		pids = append(pids, strconv.Itoa(int(o.Id)))
+	}
+	productRet, err := l.svcCtx.ProductRPC.Products(l.ctx, &product.ProductRequest{ProductIds: strings.Join(pids, ",")})
+	if err != nil {
+		return nil, err
+	}
+	var orders []*types.Order
+	for _, o := range orderRet.Orders {
+		if p, ok := productRet.Products[o.Id]; ok {
+			orders = append(orders, &types.Order{
+				OrderID:     o.Orderid,
+				ProductName: p.Name,
+			})
+		}
+	}
+	return &types.OrderListResponse{Orders: orders}, nil
 }
